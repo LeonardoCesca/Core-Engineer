fa59a0e2ee50fc200f08620534101a57fb5fb465
LeonardoCesca <leonardocescaf@gmail.com>
2019-04-21 10:46:24 -0300

    ADD modifiers

--- Aula7/src/main/java/com/leonardocesca/jtscore/aula7/Conversor.java
+++ Aula7/src/main/java/com/leonardocesca/jtscore/aula7/Conversor.java
@@ -4,37 +4,33 @@ import java.util.HashMap;
 
 public class Conversor {
 
-	private int[] numerosInteiros = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+	private int[] numerosArabicos = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 	private String[] numerosRomanos = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-	public String converteInteirosEmRomanos(int numeroInteiro) {
+	public String converteArabicosEmRomanos(int numeroArabico) {
 		String respostaNumeroRomano = "";
-		if (numeroInteiro >= 1 && numeroInteiro <= 20) {
-			for (int i = 0; i < numerosInteiros.length; i++) {
-				while (numeroInteiro >= numerosInteiros[i]) {
-					respostaNumeroRomano = respostaNumeroRomano + numerosRomanos[i];
-					numeroInteiro = numeroInteiro - numerosInteiros[i];
-				}
+		verificaIntervalo(numeroArabico);
+		for (int i = 0; i < numerosArabicos.length; i++) {
+			while (numeroArabico >= numerosArabicos[i]) {
+				respostaNumeroRomano = respostaNumeroRomano + numerosRomanos[i];
+				numeroArabico = numeroArabico - numerosArabicos[i];
 			}
-		} else {
-			System.out.println("\nO número digitado não está no intervalo permitido que é 1-20");
 		}
 		return respostaNumeroRomano;
 	}
 
 	public int converteRomanosEmInteiros(String numeroRomano) {
-		
+
 		int respostaRomano = 0;
 		int numeroDireita = 0;
-		
+
 		for (int i = numeroRomano.length() - 1; i >= 0; i--) {
 			int recebeTraducaoNumeralRomano = (int) traduzNumeroParaRomano(numeroRomano.charAt(i));
-			if (recebeTraducaoNumeralRomano >= 1 && recebeTraducaoNumeralRomano <= 20) {
+
+			if (recebeTraducaoNumeralRomano >= 1 && recebeTraducaoNumeralRomano <= 20) {			
 				respostaRomano = (int) (respostaRomano + recebeTraducaoNumeralRomano
 						* Math.signum(recebeTraducaoNumeralRomano + 0.5 - numeroDireita));
 				numeroDireita = recebeTraducaoNumeralRomano;
-			} else {
-				System.out.println("\nNúmero Romano resulta em valor fora do intervalo (1-20)");
 			}
 		}
 		return respostaRomano;
@@ -42,5 +38,15 @@ public class Conversor {
 
 	private double traduzNumeroParaRomano(char caractere) {
 		return Math.floor(Math.pow(10, "IXCM".indexOf(caractere)))+ 5 * Math.floor(Math.pow(10, "VLD".indexOf(caractere)));
+	}
+
+	private int verificaIntervalo(int numero) {
+		if (numero >= 1 && numero <= 20) {
+			return numero;
+		} else {
+			System.out.println("Numero Fora do intervalo de 1-20");
+			System.exit(0);
+		}
+		return numero;
 	}
 }
